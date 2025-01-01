f9db28e9a4cb7b912d39013808dd42f142b2e096
Jax Gun <62925754+Jgun2424@users.noreply.github.com>
2025-01-01 16:39:21 -0700

    update

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -10,7 +10,7 @@ import { useSidebar } from '@/components/ui/sidebar';
 import Image from 'next/image';
 
 
-export default function page() {
+export default function Page() {
     const { user } = useAuth()
     const { toggleSidebar, open } = useSidebar()
   
