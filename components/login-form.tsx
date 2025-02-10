9eef5b5c9db582411e6ec180a0fa2ddd340cb2b6
Jax Gun <62925754+Jgun2424@users.noreply.github.com>
2025-02-10 14:03:12 -0700

    fixes

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -26,7 +26,6 @@ export function LoginForm({
   return (
     <>
     <div className={cn("flex flex-col gap-6", className)} {...props}>
-      <form>
         <div className="flex flex-col gap-6">
           <div className="flex flex-col items-center gap-2">
             <a
@@ -52,7 +51,6 @@ export function LoginForm({
             </Button>
           </div>
         </div>
-      </form>
     </div>
     </>
   )
